dbe63ac16a423c5b949d02fae41655d807782b94
Gabriel Dibble <gdibble@gmail.com>
2011-07-31 17:32:13 -0700

    v2.4.1-1 (2009 07 28)

--- 12917.user.js
+++ 12917.user.js
@@ -99,7 +99,7 @@ var scriptHomepageURL = "http://userscripts.org/scripts/show/12917";
 	// v2.2.1	Fixed Patents search results; Fixed Web Advanced Search
 	// v2.2.2	Updated Videos; Fixed Images logo; News logo & top ads; Updated Confucius Quotes module
 	// v2.3.0	Enhanced Trends; Optimized Brute Force globals; Updated Voice
-	// v2.4.0	Enhanced Trends; Disabled update-check in Opera; Optimized Brute Force functionality
+	// v2.4.0	Enhanced Labs; Disabled update-check in Opera; Optimized Brute Force functionality
 	// v2.4.1	Improved G-logo; Fixed Images txt & Shopping grid view price; Updated News AP layout
 
 
